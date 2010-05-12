25cd59ee44da5b9be984572484f5ef37c2b7b14b
nzeemin <nzeemin@0dc07406-d219-11de-a205-5990a31c5447>
2010-05-12 21:06:41 +0000

    [+] QtBkBtl project; portability fixes.

--- trunk/src/QtBkBtl/Emulator.cpp
+++ trunk/src/QtBkBtl/Emulator.cpp
@@ -1,13 +1,10 @@
 // Emulator.cpp
 
 #include "stdafx.h"
-#include <stdio.h>
-#include <Share.h>
-#include "BKBTL.h"
+#include "main.h"
 #include "Emulator.h"
-#include "Views.h"
-#include "emubase\Emubase.h"
-#include "SoundGen.h"
+#include "emubase/Emubase.h"
+//#include "SoundGen.h"
 
 
 //////////////////////////////////////////////////////////////////////
@@ -15,12 +12,13 @@
 
 CMotherboard* g_pBoard = NULL;
 BKConfiguration g_nEmulatorConfiguration;  // Current configuration
+
+BOOL g_okEmulatorInitialized = FALSE;
 BOOL g_okEmulatorRunning = FALSE;
 
 WORD m_wEmulatorCPUBreakpoint = 0177777;
 
 BOOL m_okEmulatorSound = FALSE;
-BOOL m_okEmulatorCovox = FALSE;
 
 long m_nFrameCount = 0;
 DWORD m_dwTickCount = 0;
@@ -32,9 +30,39 @@ BYTE* g_pEmulatorChangedRam;  // RAM change flags
 WORD g_wEmulatorCpuPC = 0177777;      // Current PC value
 WORD g_wEmulatorPrevCpuPC = 0177777;  // Previous PC value
 
+const int KEYEVENT_QUEUE_SIZE = 32;
+WORD m_EmulatorKeyQueue[KEYEVENT_QUEUE_SIZE];
+int m_EmulatorKeyQueueTop = 0;
+int m_EmulatorKeyQueueBottom = 0;
+int m_EmulatorKeyQueueCount = 0;
 
-void CALLBACK Emulator_SoundGenCallback(unsigned short L, unsigned short R);
-void CALLBACK Emulator_TeletypeCallback(BYTE symbol);
+
+//////////////////////////////////////////////////////////////////////
+// Colors
+
+const DWORD ScreenView_ColorPalette[4] = {
+    0x000000, 0x0000FF, 0x00FF00, 0xFF0000
+};
+
+const DWORD ScreenView_ColorPalettes[16][4] = {
+    //                                     Palette#     01           10          11
+    0x000000, 0x0000FF, 0x00FF00, 0xFF0000,  // 00    синий   |   зеленый  |  красный
+    0x000000, 0xFFFF00, 0xFF00FF, 0xFF0000,  // 01   желтый   |  сиреневый |  красный
+    0x000000, 0x00FFFF, 0x0000FF, 0xFF00FF,  // 02   голубой  |    синий   | сиреневый
+    0x000000, 0x00FF00, 0x00FFFF, 0xFFFF00,  // 03   зеленый  |   голубой  |  желтый
+    0x000000, 0xFF00FF, 0x00FFFF, 0xFFFFFF,  // 04  сиреневый |   голубой  |   белый
+    0x000000, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF,  // 05    белый   |    белый   |   белый
+    0x000000, 0x7F0000, 0x7F0000, 0xFF0000,  // 06  темн-красн| красн-корич|  красный
+    0x000000, 0x00FF7F, 0x00FF7F, 0xFFFF00,  // 07  салатовый | светл-зелен|  желтый
+    0x000000, 0xFF00FF, 0x7F00FF, 0x7F007F,  // 08  фиолетовый| фиол-синий | сиреневый
+    0x000000, 0x00FF7F, 0x7F00FF, 0x7F0000,  // 09 светл-зелен| фиол-синий |красн-корич
+    0x000000, 0x00FF7F, 0x7F007F, 0x7F0000,  // 10  салатовый | фиолетовый |темн-красный
+    0x000000, 0x00FFFF, 0xFFFF00, 0xFF0000,  // 11   голубой  |   желтый   |  красный
+    0x000000, 0xFF0000, 0x00FF00, 0x00FFFF,  // 12   красный  |   зеленый  |  голубой
+    0x000000, 0x00FFFF, 0xFFFF00, 0xFFFFFF,  // 13   голубой  |   желтый   |   белый
+    0x000000, 0xFFFF00, 0x00FF00, 0xFFFFFF,  // 14   желтый   |   зеленый  |   белый
+    0x000000, 0x00FFFF, 0x00FF00, 0xFFFFFF,  // 15   голубой  |   зеленый  |   белый
+};
 
 
 //////////////////////////////////////////////////////////////////////
@@ -53,7 +81,7 @@ const LPCTSTR FILENAME_BKROM_BK11M_EXT  = _T("b11m_ext.rom");
 
 BOOL Emulator_LoadRomFile(LPCTSTR strFileName, BYTE* buffer, DWORD bytesToRead)
 {
-    FILE* fpRomFile = ::_tfsopen(strFileName, _T("rb"), _SH_DENYWR);
+    FILE* fpRomFile = ::_tfopen(strFileName, _T("rb"));
     if (fpRomFile == NULL)
         return FALSE;
 
@@ -72,6 +100,7 @@ BOOL Emulator_LoadRomFile(LPCTSTR strFileName, BYTE* buffer, DWORD bytesToRead)
     return TRUE;
 }
 
+
 BOOL Emulator_Init()
 {
     ASSERT(g_pBoard == NULL);
@@ -81,19 +110,23 @@ BOOL Emulator_Init()
     g_pBoard = new CMotherboard();
 
     // Allocate memory for old RAM values
-    g_pEmulatorRam = (BYTE*) ::malloc(65536);  ::memset(g_pEmulatorRam, 0, 65536);
-    g_pEmulatorChangedRam = (BYTE*) ::malloc(65536);  ::memset(g_pEmulatorChangedRam, 0, 65536);
+    g_pEmulatorRam = (BYTE*) ::malloc(65536);  memset(g_pEmulatorRam, 0, 65536);
+    g_pEmulatorChangedRam = (BYTE*) ::malloc(65536);  memset(g_pEmulatorChangedRam, 0, 65536);
 
     g_pBoard->Reset();
 
-    if (m_okEmulatorSound)
-    {
-	    SoundGen_Initialize();
-        g_pBoard->SetSoundGenCallback(Emulator_SoundGenCallback);
-    }
+    //if (m_okEmulatorSound)
+    //{
+    //    SoundGen_Initialize();
+    //    g_pBoard->SetSoundGenCallback(SoundGen_FeedDAC);
+    //}
 
-    g_pBoard->SetTeletypeCallback(Emulator_TeletypeCallback);
+    m_nUptimeFrameCount = 0;
+    m_dwEmulatorUptime = 0;
 
+    //g_pBoard->SetTeletypeCallback(Emulator_TeletypeCallback);
+
+    g_okEmulatorInitialized = TRUE;
     return TRUE;
 }
 
@@ -104,7 +137,7 @@ void Emulator_Done()
     CProcessor::Done();
 
     g_pBoard->SetSoundGenCallback(NULL);
-    SoundGen_Finalize();
+    //SoundGen_Finalize();
 
     delete g_pBoard;
     g_pBoard = NULL;
@@ -112,7 +145,10 @@ void Emulator_Done()
     // Free memory used for old RAM values
     ::free(g_pEmulatorRam);
     ::free(g_pEmulatorChangedRam);
+
+    g_okEmulatorInitialized = FALSE;
 }
+
 
 BOOL Emulator_InitConfiguration(BKConfiguration configuration)
 {
@@ -229,25 +265,18 @@ void Emulator_Start()
 {
     g_okEmulatorRunning = TRUE;
 
-    // Set title bar text
-    SetWindowText(g_hwnd, _T("BK Back to Life [run]"));
-    MainWindow_UpdateMenu();
-
     m_nFrameCount = 0;
-    m_dwTickCount = GetTickCount();
+    //m_dwTickCount = GetTickCount();
 }
 void Emulator_Stop()
 {
     g_okEmulatorRunning = FALSE;
     m_wEmulatorCPUBreakpoint = 0177777;
 
-    // Reset title bar message
-    SetWindowText(g_hwnd, _T("BK Back to Life [stop]"));
-    MainWindow_UpdateMenu();
     // Reset FPS indicator
-    MainWindow_SetStatusbarText(StatusbarPartFPS, _T(""));
+    //MainWindow_SetStatusbarText(StatusbarPartFPS, _T(""));
 
-    MainWindow_UpdateAllViews();
+    //MainWindow_UpdateAllViews();
 }
 
 void Emulator_Reset()
@@ -259,102 +288,51 @@ void Emulator_Reset()
     m_nUptimeFrameCount = 0;
     m_dwEmulatorUptime = 0;
 
-    MainWindow_UpdateAllViews();
-}
-
-void Emulator_SetCPUBreakpoint(WORD address)
-{
-    m_wEmulatorCPUBreakpoint = address;
-}
-
-BOOL Emulator_IsBreakpoint()
-{
-    WORD wCPUAddr = g_pBoard->GetCPU()->GetPC();
-    if (wCPUAddr == m_wEmulatorCPUBreakpoint)
-        return TRUE;
-    return FALSE;
-}
-
-void Emulator_SetSound(BOOL soundOnOff)
-{
-    if (m_okEmulatorSound != soundOnOff)
-    {
-        if (soundOnOff)
-        {
-            SoundGen_Initialize();
-            g_pBoard->SetSoundGenCallback(Emulator_SoundGenCallback);
-        }
-        else
-        {
-            g_pBoard->SetSoundGenCallback(NULL);
-            SoundGen_Finalize();
-        }
-    }
-
-    m_okEmulatorSound = soundOnOff;
-}
-
-void Emulator_SetCovox(BOOL covoxOnOff)
-{
-    m_okEmulatorCovox = covoxOnOff;
+    //MainWindow_UpdateAllViews();
 }
 
 int Emulator_SystemFrame()
 {
     g_pBoard->SetCPUBreakpoint(m_wEmulatorCPUBreakpoint);
 
-    ScreenView_ScanKeyboard();
-    ScreenView_ProcessKeyboard();
+    //ScreenView_ScanKeyboard();
+    Emulator_ProcessKeyEvent();
     
 	if (!g_pBoard->SystemFrame())
         return 0;
 
-	// Calculate frames per second
-	m_nFrameCount++;
-	DWORD dwCurrentTicks = GetTickCount();
-	long nTicksElapsed = dwCurrentTicks - m_dwTickCount;
-	if (nTicksElapsed >= 1200)
-	{
-		double dFramesPerSecond = m_nFrameCount * 1000.0 / nTicksElapsed;
-		TCHAR buffer[16];
-		swprintf_s(buffer, 16, _T("FPS: %05.2f"), dFramesPerSecond);
-		MainWindow_SetStatusbarText(StatusbarPartFPS, buffer);
+    //// Calculate frames per second
+    //m_nFrameCount++;
+    //DWORD dwCurrentTicks = GetTickCount();
+    //long nTicksElapsed = dwCurrentTicks - m_dwTickCount;
+    //if (nTicksElapsed >= 1200)
+    //{
+    //	double dFramesPerSecond = m_nFrameCount * 1000.0 / nTicksElapsed;
+    //	TCHAR buffer[16];
+    //	_stprintf(buffer, _T("FPS: %05.2f"), dFramesPerSecond);
+    //    //MainWindow_SetStatusbarText(StatusbarPartFPS, buffer);
 
-		m_nFrameCount = 0;
-		m_dwTickCount = dwCurrentTicks;
-	}
+    //	m_nFrameCount = 0;
+    //	m_dwTickCount = dwCurrentTicks;
+    //}
 
-	// Calculate emulator uptime (25 frames per second)
-	m_nUptimeFrameCount++;
-	if (m_nUptimeFrameCount >= 25)
-	{
-		m_dwEmulatorUptime++;
-		m_nUptimeFrameCount = 0;
+    //// Calculate emulator uptime (25 frames per second)
+    //m_nUptimeFrameCount++;
+    //if (m_nUptimeFrameCount >= 25)
+    //{
+    //	m_dwEmulatorUptime++;
+    //	m_nUptimeFrameCount = 0;
 
-		int seconds = (int) (m_dwEmulatorUptime % 60);
-		int minutes = (int) (m_dwEmulatorUptime / 60 % 60);
-		int hours   = (int) (m_dwEmulatorUptime / 3600 % 60);
+    //	int seconds = (int) (m_dwEmulatorUptime % 60);
+    //	int minutes = (int) (m_dwEmulatorUptime / 60 % 60);
+    //	int hours   = (int) (m_dwEmulatorUptime / 3600 % 60);
 
-		TCHAR buffer[20];
-		swprintf_s(buffer, 20, _T("Uptime: %02d:%02d:%02d"), hours, minutes, seconds);
-		MainWindow_SetStatusbarText(StatusbarPartUptime, buffer);
-	}
+    //	TCHAR buffer[20];
+    //	_stprintf(buffer, _T("Uptime: %02d:%02d:%02d"), hours, minutes, seconds);
+    //    //MainWindow_SetStatusbarText(StatusbarPartUptime, buffer);
+    //}
 
     return 1;
-}
-
-void CALLBACK Emulator_SoundGenCallback(unsigned short L, unsigned short R)
-{
-    if (m_okEmulatorCovox)
-    {
-        // Get lower byte from printer port output register
-        unsigned short data = g_pBoard->GetPrinterOutPort() & 0xff;
-        // Merge with channel data
-        L += (data << 7);
-        R += (data << 7);
-    }
-
-    SoundGen_FeedDAC(L, R);
 }
 
 // Update cached values after Run or Step
@@ -389,111 +367,108 @@ WORD Emulator_GetChangeRamStatus(WORD address)
     return *((WORD*)(g_pEmulatorChangedRam + address));
 }
 
-void CALLBACK Emulator_TeletypeCallback(BYTE symbol)
+void Emulator_PrepareScreenRGB32(void* pImageBits, ScreenViewMode screenMode)
 {
-    if (g_hwndTeletype != (HWND) INVALID_HANDLE_VALUE)
+    if (pImageBits == NULL) return;
+    if (!g_okEmulatorInitialized) return;
+
+    // Get scroll value
+    WORD scroll = g_pBoard->GetPortView(0177664);
+    BOOL okSmallScreen = ((scroll & 01000) == 0);
+    scroll &= 0377;
+    scroll = (scroll >= 0330) ? scroll - 0330 : 050 + scroll;
+
+    // Get palette
+    DWORD* pPalette;
+    //if ((g_nEmulatorConfiguration & BK_COPT_BK0011) == 0)
+        pPalette = (DWORD*)ScreenView_ColorPalette;
+    //else
+    //    pPalette = (DWORD*)ScreenView_ColorPalettes[g_pBoard->GetPalette()];
+
+    const BYTE* pVideoBuffer = g_pBoard->GetVideoBuffer();
+    ASSERT(pVideoBuffer != NULL);
+
+    // Render to bitmap
+    int linesToShow = okSmallScreen ? 64 : 256;
+    for (int y = 0; y < linesToShow; y++)
     {
-        if (symbol >= 32 || symbol == 13 || symbol == 10)
+        int yy = (y + scroll) & 0377;
+        const WORD* pVideo = (WORD*)(pVideoBuffer + yy * 0100);
+        DWORD* pBits = (DWORD*)pImageBits + y * 512;
+        for (int x = 0; x < 512 / 16; x++)
         {
-            TeletypeView_OutputSymbol((TCHAR)symbol);
+            WORD src = *pVideo;
+
+            if (screenMode == BlackWhiteScreen)  // Black and white mode 512 x 256
+            {
+                for (int bit = 0; bit < 16; bit++)
+                {
+                    DWORD color = (src & 1) ? 0x0ffffff : 0;
+                    *pBits = color;
+                    pBits++;
+                    src = src >> 1;
+                }
+            }
+            else  // Color mode 256 x 256
+            {
+                for (int bit = 0; bit < 16; bit += 2)
+                {
+                    DWORD color = pPalette[src & 3];
+                    *pBits = color;
+                    pBits++;
+                    *pBits = color;
+                    pBits++;
+                    src = src >> 2;
+                }
+            }
+
+            pVideo++;
         }
-        else
-        {
-            TCHAR buffer[32];
-            _snwprintf_s(buffer, 32, _T("<%02x>"), symbol);
-            TeletypeView_Output(buffer);
-        }
+    }
+
+    if (okSmallScreen)
+    {
+        memset((DWORD*)pImageBits, 64 * 512, (256 - 64) * 512 * sizeof(DWORD));
     }
 }
 
-
-//////////////////////////////////////////////////////////////////////
-//
-// Emulator image format - see CMotherboard::SaveToImage()
-// Image header format (32 bytes):
-//   4 bytes        BK_IMAGE_HEADER1
-//   4 bytes        BK_IMAGE_HEADER2
-//   4 bytes        BK_IMAGE_VERSION
-//   4 bytes        BK_IMAGE_SIZE
-//   4 bytes        BK uptime
-//   12 bytes       Not used
-
-void Emulator_SaveImage(LPCTSTR sFilePath)
+void Emulator_KeyEvent(BYTE keyscan, BOOL pressed)
 {
-    // Create file
-    HANDLE hFile = CreateFile(sFilePath,
-            GENERIC_WRITE, FILE_SHARE_READ, NULL,
-            CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-    if (hFile == INVALID_HANDLE_VALUE)
-    {
-        AlertWarning(_T("Failed to save image file."));
-        return;
-    }
+    if (m_EmulatorKeyQueueCount == KEYEVENT_QUEUE_SIZE) return;  // Full queue
 
-    // Allocate memory
-    BYTE* pImage = (BYTE*) ::malloc(BKIMAGE_SIZE);  memset(pImage, 0, BKIMAGE_SIZE);
-    ::memset(pImage, 0, BKIMAGE_SIZE);
-    // Prepare header
-    DWORD* pHeader = (DWORD*) pImage;
-    *pHeader++ = BKIMAGE_HEADER1;
-    *pHeader++ = BKIMAGE_HEADER2;
-    *pHeader++ = BKIMAGE_VERSION;
-    *pHeader++ = BKIMAGE_SIZE;
-    // Store emulator state to the image
-    //g_pBoard->SaveToImage(pImage);
-    *(DWORD*)(pImage + 16) = m_dwEmulatorUptime;
+    WORD keyevent = MAKEWORD(keyscan, pressed ? 128 : 0);
 
-    // Save image to the file
-    DWORD dwBytesWritten = 0;
-    WriteFile(hFile, pImage, BKIMAGE_SIZE, &dwBytesWritten, NULL);
-    //TODO: Check if dwBytesWritten != BKIMAGE_SIZE
-
-    // Free memory, close file
-    ::free(pImage);
-    CloseHandle(hFile);
+    m_EmulatorKeyQueue[m_EmulatorKeyQueueTop] = keyevent;
+    m_EmulatorKeyQueueTop++;
+    if (m_EmulatorKeyQueueTop >= KEYEVENT_QUEUE_SIZE)
+        m_EmulatorKeyQueueTop = 0;
+    m_EmulatorKeyQueueCount++;
 }
 
-void Emulator_LoadImage(LPCTSTR sFilePath)
+WORD Emulator_GetKeyEventFromQueue()
 {
-    // Open file
-    HANDLE hFile = CreateFile(sFilePath,
-            GENERIC_READ, FILE_SHARE_READ, NULL,
-            OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-    if (hFile == INVALID_HANDLE_VALUE)
+    if (m_EmulatorKeyQueueCount == 0) return 0;  // Empty queue
+
+    WORD keyevent = m_EmulatorKeyQueue[m_EmulatorKeyQueueBottom];
+    m_EmulatorKeyQueueBottom++;
+    if (m_EmulatorKeyQueueBottom >= KEYEVENT_QUEUE_SIZE)
+        m_EmulatorKeyQueueBottom = 0;
+    m_EmulatorKeyQueueCount--;
+
+    return keyevent;
+}
+
+void Emulator_ProcessKeyEvent()
+{
+    // Process next event in the keyboard queue
+    WORD keyevent = Emulator_GetKeyEventFromQueue();
+    if (keyevent != 0)
     {
-        AlertWarning(_T("Failed to load image file."));
-        return;
+        BOOL pressed = ((keyevent & 0x8000) != 0);
+        BOOL ctrl = ((keyevent & 0x4000) != 0);
+        BYTE bkscan = LOBYTE(keyevent);
+        g_pBoard->KeyboardEvent(bkscan, pressed, ctrl);
     }
-
-    // Read header
-    DWORD bufHeader[BKIMAGE_HEADER_SIZE / sizeof(DWORD)];
-    DWORD dwBytesRead = 0;
-    ReadFile(hFile, bufHeader, BKIMAGE_HEADER_SIZE, &dwBytesRead, NULL);
-    //TODO: Check if dwBytesRead != BKIMAGE_HEADER_SIZE
-    
-    //TODO: Check version and size
-
-    // Allocate memory
-    BYTE* pImage = (BYTE*) ::malloc(BKIMAGE_SIZE);  ::memset(pImage, 0, BKIMAGE_SIZE);
-
-    // Read image
-    SetFilePointer(hFile, 0, NULL, FILE_BEGIN);
-    dwBytesRead = 0;
-    ReadFile(hFile, pImage, BKIMAGE_SIZE, &dwBytesRead, NULL);
-    //TODO: Check if dwBytesRead != BKIMAGE_SIZE
-
-    // Restore emulator state from the image
-    //g_pBoard->LoadFromImage(pImage);
-
-    m_dwEmulatorUptime = *(DWORD*)(pImage + 16);
-
-    // Free memory, close file
-    ::free(pImage);
-    CloseHandle(hFile);
-
-    g_okEmulatorRunning = FALSE;
-
-    MainWindow_UpdateAllViews();
 }
 
 
