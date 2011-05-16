fdf1a4cd2878993b67dd55da8a6434fff326862c
nzeemin <nzeemin@0dc07406-d219-11de-a205-5990a31c5447>
2011-05-16 19:33:44 +0000

    [*] BKBTL ans SdlBkBtl: switch back to VS2005; SdlBkBtl: PSP adaptation (not finished).

--- trunk/src/SdlBkBtl/Settings.cpp
+++ trunk/src/SdlBkBtl/Settings.cpp
@@ -5,7 +5,12 @@
 /////////////////////////////////////////////////////////////////////////////
 
 
-void Settings_ParseKeyValue(const char* pSectName, const char* pKey, const char* pValue)
+void Settings_ParseSection(const char* pSectName)
+{
+    //TODO
+}
+
+void Settings_ParseKeyValue(const char* pKey, const char* pValue)
 {
     //TODO
 }
@@ -42,6 +47,7 @@ void Settings_ParseSettings(char* pText)
             {
                 *pText++ = 0;  // Mark end of the section name
                 pSection = pNewSection;
+                Settings_ParseSection(pSection);
             }
 
             while (*pText != 0 && *pText != '\n' && *pText != '\r') pText++;  // Skip to the end of line
@@ -63,17 +69,22 @@ void Settings_ParseSettings(char* pText)
         while (*pText == ' ' || *pText == '\t') pText++;  // Skip spaces and tabs after '='
         if (*pText == 0 || *pText == '\n' || *pText == '\r')
         {
-            Settings_ParseKeyValue(pSection, pKey, NULL);
+            Settings_ParseKeyValue(pKey, NULL);
             continue;
         }
         char* pValue = pText;
         while (*pText != 0 && *pText != '\n' && *pText != '\r') pText++;  // Seek to the end of line
         while (*(pText-1) == ' ' || *(pText-1) == '\t') pText--;  // Trailing spaces and tabs
         if (*pText != 0) *pText++ = 0;  // Mark end of the value
-        Settings_ParseKeyValue(pSection, pKey, pValue);
+        Settings_ParseKeyValue(pKey, pValue);
 
         while (*pText != 0 && *pText != '\n' && *pText != '\r') pText++;  // Skip to the end of line
     }
+}
+
+void Settings_ParseIniFile(const char* sFileName)
+{
+    //TODO
 }
 
 
